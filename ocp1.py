b5afb33255eed7093c40ec3edb7e80f2eddff80b
mabunada84-stack <mabunada84@gmail.com>
2026-06-16 23:34:55 +0100

    Update ocp1.py

--- ocp1.py
+++ ocp1.py
@@ -532,14 +532,14 @@ def main():
 
     # ---- Cellule style ----
     def ks(v,c):
-        try: val=float(v)
+         try: val=float(v)
         except Exception: return ""
         if c in ["OT préparation <1 mois","OT planification <1 mois","OT exécution <1 mois"]:
             return "background:#c6efce;color:#006100;font-weight:600" if val>=80 else ("background:#ffeb9c;color:#9c6500;font-weight:600" if val>=75 else "background:#ffc7ce;color:#9c0006;font-weight:600")
         if c in ["OT préparation 1mois< <3mois","OT planification 1mois< <3mois","OT exécution 1mois< <3mois"]:
             return "background:#c6efce;color:#006100;font-weight:600" if val<=15 else "background:#ffc7ce;color:#9c0006;font-weight:600"
         if c in ["OT préparation >3 mois","OT planification >3 mois","OT exécution >3 mois"]:
-            return "background:#c6efce;color:#006100;font-weight:600" if val<=5 else "background:#ffc7ce;color:#9c0006;font-weight:600")
+            return "background:#c6efce;color:#006100;font-weight:600" if val<=5 else "background:#ffc7ce;color:#9c0006;font-weight:600"
         if c=="TAUX_REALISATION_CORRECTIF/PT":
             return "background:#c6efce;color:#006100;font-weight:600" if val>=85 else ("background:#ffeb9c;color:#9c6500;font-weight:600" if val>=80 else "background:#ffc7ce;color:#9c0006;font-weight:600")
         if c=="appel avis approuvé":
